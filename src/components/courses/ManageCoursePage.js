80bcedf4f594b8641d689a6f3cd1795e957ef246
Paolo <paoloagva@gmail.com>
2022-11-26 23:10:05 +0100

    created actions for create update forms

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -1,10 +1,20 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { connect } from "react-redux";
 import { loadCourses } from "../../redux/actions/courseActions";
 import { loadAuthors } from "../../redux/actions/authorActions";
 import PropTypes from "prop-types";
+import CourseForm from "./CourseForm";
+import { newCourse } from "../../../tools/mockData";
 
-function ManageCoursesPage({ courses, authors, loadAuthors, loadCourses }) {
+function ManageCoursesPage({
+    courses,
+    authors,
+    loadAuthors,
+    loadCourses,
+    ...props
+}) {
+    const [course, setCourse] = useState({ ...props.course });
+    const [errors, setErrors] = useState({});
     useEffect(() => {
         if (courses.length === 0) {
             loadCourses().catch((error) => {
@@ -19,14 +29,26 @@ function ManageCoursesPage({ courses, authors, loadAuthors, loadCourses }) {
         }
     }, []);
 
+    function handleChange(event) {
+        const { name, value } = event.target;
+        setCourse((prevCourse) => ({
+            ...prevCourse,
+            [name]: name === "authorId" ? parseInt(value, 10) : value,
+        }));
+    }
+
     return (
-        <>
-            <h2>Manage Course</h2>
-        </>
+        <CourseForm
+            course={course}
+            errors={errors}
+            authors={authors}
+            onChange={handleChange}
+        />
     );
 }
 
 ManageCoursesPage.propTypes = {
+    course: PropTypes.object.isRequired,
     courses: PropTypes.array.isRequired,
     authors: PropTypes.array.isRequired,
     loadCourses: PropTypes.func.isRequired,
@@ -35,6 +57,7 @@ ManageCoursesPage.propTypes = {
 
 function mapStateToProps(state) {
     return {
+        course: newCourse,
         courses: state.courses,
         authors: state.authors,
     };
--- src/redux/actions/courseActions.js
+++ src/redux/actions/courseActions.js
@@ -1,12 +1,20 @@
 import * as types from "./actionTypes";
 import * as courseApi from "../../api/courseApi";
 
-export function createCourse(course) {
-    return { type: types.CREATE_COURSE, course }; // object shorthand symtax
-}
+// export function createCourse(course) { // not necessary anymore
+//     return { type: types.CREATE_COURSE, course }; // object shorthand symtax
+// }
 
 export function loadCoursesSuccess(courses) {
     return { type: types.LOAD_COURSES_SUCCESS, courses }; // object shorthand symtax
+}
+
+export function createCourseSucces(course) {
+    return { type: types.CREATE_COURSE_SUCCESS, course }; // object shorthand symtax
+}
+
+export function saveCourseSuccess(course) {
+    return { type: types.UPDATE_COURSE_SUCCESS, course }; // object shorthand symtax
 }
 
 export function loadCourses() {
@@ -15,6 +23,21 @@ export function loadCourses() {
             .getCourses()
             .then((courses) => {
                 dispatch(loadCoursesSuccess(courses));
+            })
+            .catch((error) => {
+                throw error;
+            });
+    };
+}
+
+export function saveCourse(course) {
+    return function (dispatch, getState) {
+        return courseApi
+            .saveCourse()
+            .then((savedCourse) => {
+                course.id
+                    ? dispatch(saveCourseSuccess(course))
+                    : dispatch(createCourseSucces(course));
             })
             .catch((error) => {
                 throw error;
